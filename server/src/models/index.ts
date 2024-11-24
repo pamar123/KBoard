302bc8179a5123bd9cb898ded450ba22ee58cb1b
Amar Patel <pamar@umich.edu>
2024-11-23 22:59:43 -0500

    Add SSL configuration for Render database connection

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,3 +1,4 @@
+
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -6,7 +7,15 @@ import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
+  ? new Sequelize(process.env.DB_URL, {
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        },
+        decimalNumbers: true,
+      },
+    })
   : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'postgres',
